828b21b194d4736067cffc0576ea67dc95d03554
Bernardo <bteisceira@gmail.com>
2023-09-16 19:18:15 -0300

    Add lambda file & module calls

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -15,5 +15,72 @@ module "website-bucket" {
   source = "github.com/bertei/CloudResume-TerraformModules.git//s3"
   #source  = "/Users/bernardo.teisceira/Desktop/Personal-repos/CloudResume-TerraformModules/s3"
 
-  bucket_name = "bernatei-website-2"
+  bucket_name = "bernatei-website"
 }
+
+module "website-acm-certificate" {
+  source = "github.com/bertei/CloudResume-TerraformModules.git//acm"
+  #source = "/Users/bernardo.teisceira/Desktop/Personal-repos/CloudResume-TerraformModules/acm"
+}
+
+module "website-cdn" {
+  source = "github.com/bertei/CloudResume-TerraformModules.git//cdn"
+  #source = "/Users/bernardo.teisceira/Desktop/Personal-repos/CloudResume-TerraformModules/cdn"
+
+  ##Variables defined by outputs
+  domain_name = module.website-bucket.bucket_regional_domain_name
+  bucket_id   = module.website-bucket.bucket_id
+  bucket_arn  = module.website-bucket.bucket_arn
+  acm_arn     = module.website-acm-certificate.acm_arn
+
+
+  depends_on = [
+    module.website-bucket,
+    module.website-acm-certificate
+  ]
+}
+
+module "website-dynamodb" {
+  source  = "github.com/bertei/CloudResume-TerraformModules.git//dynamodb"
+  #source = "/Users/bernardo.teisceira/Desktop/Personal-repos/CloudResume-TerraformModules/dynamodb"
+
+  table_name = "ViewCounter"
+}
+
+module "website-lambda" {
+  source  = "github.com/bertei/CloudResume-TerraformModules.git//lambda"
+  #source = "/Users/bernardo.teisceira/Desktop/Personal-repos/CloudResume-TerraformModules/lambda"
+
+  lambda_function_name = "Website-Lambda"
+  lambda_role_name     = "ViewCounter-Role"
+
+  ##Variables defined by outputs
+  table_arn            = module.website-dynamodb.table_arn
+
+  depends_on = [ 
+    module.website-dynamodb
+  ]
+}
+
+module "website-apigw" {
+  source  = "github.com/bertei/CloudResume-TerraformModules.git//apigw"
+  #source = "/Users/bernardo.teisceira/Desktop/Personal-repos/CloudResume-TerraformModules/apigw"
+
+  apigw_name  = "Website-API"
+
+  ##Variable defined by outputs
+  lambda_invoke_arn    = module.website-lambda.lambda_invoke_arn
+  lambda_function_name = module.website-lambda.lambda_function_name
+
+  depends_on = [ 
+    module.website-lambda
+   ]
+}
+
+#Used for testing purposes locally. Not using it online because i purchased/created the HZ via console.
+#I won't import it. But i was able to create the A record hardcoding the zone_id. Not uploading it for security purposes.
+#module "website-r53" {
+#  source = "/Users/bernardo.teisceira/Desktop/Personal-repos/CloudResume-TerraformModules/r53"
+#
+#  cdn_name = module.website-cdn.cdn_name
+#}
--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,8 +1,40 @@
+## S3 Outputs ##
 output "bucket_arn" {
   value = module.website-bucket.bucket_arn
 }
 
-output "bucket_domain" {
-  description = "Domain name of the bucket"
-  value       = module.website-bucket.bucket_domain
+output "bucket_id" {
+  description = "ID of the bucket"
+  value       = module.website-bucket.bucket_id
+}
+
+output "bucket_regional_domain_name" {
+  description = "The bucket domain name including the region name."
+  value       = module.website-bucket.bucket_regional_domain_name    
+}
+
+## ACM Outputs ##
+output "acm_arn" {
+  value = module.website-acm-certificate.acm_arn
+}
+
+## CDN Outputs ##
+output "cdn_domain_name" {
+  value = module.website-cdn.cdn_name
+}
+
+## DynamoDB Outputs ##
+output "table_arn" {
+  value = module.website-dynamodb
+}
+
+## Lambda Outputs ##
+output "lambda_arn" {
+  value = module.website-lambda.lambda_arn
+}
+output "lambda_invoke_arn" {
+  value = module.website-lambda.lambda_invoke_arn
+}
+output "lambda_function_name" {
+  value = module.website-lambda.lambda_function_name
 }
