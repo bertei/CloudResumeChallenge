80b40d2820c187c84fae3271f940cdd36a17b502
Bernardo <bteisceira@gmail.com>
2023-09-17 20:27:34 -0300

    Dummy commit to trigger workflow

--- Website/script.js
+++ Website/script.js
@@ -1,4 +1,13 @@
-// Some assembly required, batteries sold separately
-
-// Inspiration
-// https://www.pinterest.com/pin/556687203921295406/
+function visitcount() {
+    return fetch("https://vuqzcgsd40.execute-api.us-east-1.amazonaws.com/dev/Test_lambda").then(function(response) {
+    return response.json();
+    }).then(function(json) {
+        return json;
+    });
+    }
+  
+    visitcount().then(function(result) {
+        const viewCountPlaceholder = document.getElementById("viewCountPlaceholder");
+        viewCountPlaceholder.textContent = result;
+        console.log(result);
+    });
