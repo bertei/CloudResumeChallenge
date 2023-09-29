ebf745a14d1ef40963db7b5bd0e3e271764d111b
Bernardo <bteisceira@gmail.com>
2023-09-29 00:03:43 -0300

    Feat: refactor html, css and script

--- Website/script.js
+++ Website/script.js
@@ -3,14 +3,13 @@
 //Once the visit-number is returned, the function sets the number into the html element. Also logs the result to the console.
 function visitcount() {
     return fetch("https://vuqzcgsd40.execute-api.us-east-1.amazonaws.com/dev/Website-Lambda").then(function(response) {
-    return response.json();
+        return response.json();
     }).then(function(json) {
         return json;
     });
-    }
-  
-    visitcount().then(function(result) {
-        const viewCountPlaceholder = document.getElementById("viewCountPlaceholder");
-        viewCountPlaceholder.textContent = result;
-        console.log(result);
-    });
+}
+
+visitcount().then(function(result) {
+    const viewCountPlaceholder = document.getElementById("viewCountPlaceholder");
+    viewCountPlaceholder.textContent = "View-Counter: " + result;
+});
